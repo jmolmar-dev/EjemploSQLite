717f73f204f988d9e40c4aec97b46f71a60a466b
Pablo Carrilo <pcargar144@iescarrillo.es>
2024-10-30 10:36:55 +0100

    Encriptar password

--- app/src/main/java/es/iescarrillo/android/ejemplosqlite/activities/MainActivity.java
+++ app/src/main/java/es/iescarrillo/android/ejemplosqlite/activities/MainActivity.java
@@ -2,9 +2,11 @@ package es.iescarrillo.android.ejemplosqlite.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -14,6 +16,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 import java.util.List;
 import java.util.Random;
 
@@ -21,18 +25,24 @@ import es.iescarrillo.android.ejemplosqlite.R;
 import es.iescarrillo.android.ejemplosqlite.adapters.PersonAdapter;
 import es.iescarrillo.android.ejemplosqlite.models.Address;
 import es.iescarrillo.android.ejemplosqlite.models.Person;
+import es.iescarrillo.android.ejemplosqlite.models.UserAccount;
+import es.iescarrillo.android.ejemplosqlite.services.CarService;
 import es.iescarrillo.android.ejemplosqlite.services.PersonService;
+import es.iescarrillo.android.ejemplosqlite.services.UserAccountService;
 
 public class MainActivity extends AppCompatActivity {
 
     // Services
     private PersonService personService;
+    private UserAccountService userAccountService;
+    private CarService carService;
 
     // Compoments
     private ListView lvPeople;
-    private Button btnInsert;
+    private Button btnInsert, btnCheck;
     private PersonAdapter personAdapter;
     private List<Person> personList;
+    private EditText etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,12 +57,36 @@ public class MainActivity extends AppCompatActivity {
 
         // Inicializamos el servicio
         personService = new PersonService(getApplication());
+        userAccountService = new UserAccountService(getApplication());
+        carService = new CarService(getApplication());
 
         // Cargamos los compoentes
         lvPeople = findViewById(R.id.lvPerson);
         btnInsert = findViewById(R.id.btnInsertPerson);
+        etPassword = findViewById(R.id.etPassword);
+        btnCheck = findViewById(R.id.btnCheckPassword);
 
         refresh();
+
+        btnCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Boolean findOne = false;
+                List<UserAccount> userAccountList = userAccountService.getAll();
+                for(UserAccount u:userAccountList){
+                    if(BCrypt.checkpw(etPassword.getText().toString(), u.getPassword())){
+                        Toast.makeText(getApplicationContext(), "El usuario es " + u.getUsername(), Toast.LENGTH_SHORT).show();
+                        findOne = true;
+                        break;
+                    }
+                }
+
+                if(!findOne){
+                    Toast.makeText(getApplicationContext(), "No ha encontrado ninguno", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
 
 
         btnInsert.setOnClickListener(new View.OnClickListener() {
@@ -60,6 +94,20 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Random random = new Random();
                 Integer randomValue = random.nextInt(100);
+
+                // Creamos el UserAccount
+                UserAccount userAccount = new UserAccount();
+                userAccount.setUsername("pablo"+randomValue);
+
+                // Encriptar la password
+                // BCrypt.hashpw(parametr1 => la contraseña a encriptar, parametro2 => coste de la encriptación)
+                String password = BCrypt.hashpw("Usuario" + randomValue + "?", BCrypt.gensalt(5));
+                Log.i("Password encrypt", password);
+
+                userAccount.setPassword(password);
+                long userAccountId = userAccountService.insert(userAccount);
+
+
                 Person p = new Person();
                 p.setName("Pablo " + randomValue);
                 p.setSurname("Carrillo");
@@ -72,6 +120,7 @@ public class MainActivity extends AppCompatActivity {
                 address.setStreetNumber(String.valueOf(randomValue));
 
                 p.setAddress(address);
+                p.setUserAccountId(userAccountId);
 
                 // Almacenamos en una variable el ID de la persona creada
                 long idPersonSaved = personService.insertPerson(p);
